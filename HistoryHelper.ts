be48574dc8b1d83153fbd2c36beaffd13965d3c0
angular-moon <xiaoxuss@163.com>
2019-09-02 10:36:08 +0800

    init

---
+++ HistoryHelper.ts
@@ -0,0 +1,40 @@
+import { merge, get } from 'lodash';
+
+// eslint-disable-next-line no-restricted-globals
+const { history } = top;
+
+// 持久化搜索条件key前缀
+const STORE_KEY_PREFIX = '__hh';
+
+export default class HistoryHelper {
+  private storeKey: string;
+  private history: History;
+
+  constructor(storeKey?: string, _history?: History) {
+    this.storeKey = storeKey ? `${STORE_KEY_PREFIX}_${storeKey}` : STORE_KEY_PREFIX;
+    this.history = _history || history;
+  }
+
+  setState(state: any) {
+    const historyState = this.history.state || {};
+    historyState[this.storeKey] = state;
+    this.history.replaceState(historyState, '');
+  }
+
+  getState(): object {
+    return this.history.state && this.history.state[this.storeKey];
+  }
+
+  mergeState(...state: object[]) {
+    const mergedState = merge({}, ...this.history.state[this.storeKey], ...state);
+    this.setState(mergedState);
+  }
+
+  clearState() {
+    this.setState(null);
+  }
+
+  getValue(path: string, defaultValue: any = null) {
+    return get(this.getState(), path, defaultValue);
+  }
+}
--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # history-helper
 history-helper
+
+[![NPM](https://img.shields.io/npm/v/history-helper.svg)](https://www.npmjs.com/package/history-helper)
+
+## Install
+
+```bash
+yarn add history-helper
+```
+
+## Usage
+
+```ts
+import HistoryHelper from 'history-helper';
+
+/**
+ * storeKey [string] optional - 设置state[key]
+ * storeHistory [History] optional - 设置指定的history
+ **/
+const historyHelper = new HistoryHelper(storeKey, storeHistory);
+
+/** apis **/
+
+historyHelper.setState(state: any);
+
+historyHelper.getState()
+
+historyHelper.mergeState(...state: object[]);
+
+historyHelper.clearState();
+
+historyHelper.getValue(path: string, defaultValue: any = null);
+
+```
